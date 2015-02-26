bfa1278576b20df473e8ae3cbb66864de94c1668
Robby Ranshous <rranshous@gmail.com>
2015-02-25 21:45:40 -0500

    blog identifier working

--- blog/identifier/worker.rb
+++ blog/identifier/worker.rb
@@ -3,14 +3,14 @@ require 'streamworker'
 name 'new-blog-notifier'
 handle 'tumblr' => 'observed-blog' do |state, event, redis|
   blog_href = event[:body]['href']
+  log "handling: #{blog_href}"
   if redis.sismember('seen', blog_href)
-    puts "SEEN, skipping: #{blog_href}"
+    log "seen, skipping: #{blog_href}"
     next
   end
-  puts "NEW: #{blog_href}"
+  log "new: #{blog_href}"
   blog_data = { href: blog_href, post: event[:body]['post'] }
-  emit 'new-images', 'tumblr-image', blog_data
+  log "emitting new-blog-observed #{blog_href}"
+  emit 'tumblr', 'new-blog-observed', blog_data
   redis.sadd('seen', blog_href)
 end
-
-
