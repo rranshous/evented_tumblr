62c7a9cfb9664f38afa7702e066e36e4c425a2f9
Robby Ranshous <rranshous@gmail.com>
2015-02-25 23:11:49 -0500

    post historical scraper working

--- post/observer/historical/worker.rb
+++ post/observer/historical/worker.rb
@@ -6,54 +6,60 @@ require 'uri'
 require 'feedjira'
 require 'streamworker'
 
-handle 'new-blogs' do |state, event|
+name 'historical-post-scraper'
+
+handle 'tumblr' => 'new-blog-observed' do |state, event, redis|
   join = lambda { |*args|
     args.map { |arg| arg.gsub(%r{^/*(.*?)/*$}, '\1') }.join("/")
   }
-  state[:seen] ||= []
   blog_href = event[:body]['href']
   if blog_href.nil? || blog_href.chomp == ''
-    puts "BAD BLOG HREF: #{blog_href}"
+    log "bad blog href: #{blog_href}"
     next
   end
-  if state[:seen].include? blog_href
-    puts "SEEN BLOG: #{blog_href}"
+  if redis.sismember 'finished', blog_href
+    log "scrape finished, skipping: #{blog_href}"
     next
   end
-  state[:seen] << blog_href
-  puts "BLOG HREF: #{blog_href}"
+  log "handling: #{blog_href}"
   last_page = nil
   post_count = 0
   (1..1000).each do |page_number|
     urls = [join.call(blog_href,'/page/',"/#{page_number}/",'rss').to_s]
-    puts "URL: #{urls.last}"
+    log "url: #{urls.last}"
     feed = Feedjira::Feed.fetch_and_parse(urls)[urls.first]
     if feed.is_a?(Fixnum)
-      puts "BAD FEED: #{feed}"
+      log "bad page feed: #{feed}"
     else
       if feed.entries.length == 0
-        puts "END OF BLOG [#{blog_href}]: #{page_number}"
+        log "end of blog found [#{blog_href}]: #{page_number}"
         break
       end
       feed.entries.each do |entry|
-        puts "HANDLING POST [#{post_count+1}]: #{entry.url}"
+        if redis.sismember 'entry_seen', entry.url
+          log "handled post, skipping: #{entry.url}"
+          next
+        end
+        log "handling post [#{post_count+1}]: #{entry.url}"
         post_data = {
           'href' => entry.url,
           'blog' => { 'href' => feed.url },
           'timestamp' => entry.published.iso8601
         }
-        puts "EMIT ['tumblr'|'observed_post']: #{post_data}"
+        log "emit observed_post: #{entry.url}"
         emit 'tumblr', 'observed-post', post_data
+        redis.sadd 'entry_seen', entry.url
         post_count += 1
       end
     end
     last_page = page_number
   end
-  emit 'blogs', 'scraped', {
+  emit 'tumblr', 'finished-scrape', {
     'href' => blog_href,
     'num_pages_scraped' => last_page,
     'num_posts_observed' => post_count
   }
+  redis.sadd 'finished', blog_href
 end
 
 at_exit { puts "ERROR: #{$!}" unless $!.nil? }
